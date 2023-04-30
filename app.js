380237db062ef54a7004b10ff04226a3a7d0c01c
Karl <toom.karl42@gmail.com>
2023-04-30 15:56:23 +0300

    added dropdown menu and fixed sum bugs

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-const nav = document.querySelector("nav")
-const sticky = nav.offsetTop
+const nav = document.querySelector("nav");
+const sticky = nav.offsetTop;
 
-window.onscroll = function(){
-    if (window.scrollY > sticky){
-        nav.classList.add("sticky")
-    }
-    else{
-        nav.classList.remove("sticky")
-    }
-}
+window.onscroll = function () {
+  if (window.scrollY > sticky) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+};
+
+const menuIcon = document.querySelector('.nav__link--menu');
+const navLinks = document.querySelector('.nav__links');
+
+menuIcon.addEventListener('click', function() {
+  navLinks.classList.toggle('active');
+});
+
+document.addEventListener('click', function(event) {
+  const isClickInside = menuIcon.contains(event.target) || navLinks.contains(event.target);
+  if (!isClickInside) {
+    navLinks.classList.remove('active');
+  }
+});
+const menuLink = document.querySelector('.nav__link--menu');
+
+menuLink.addEventListener('click', function(event) {
+  event.preventDefault();
+  
+});
