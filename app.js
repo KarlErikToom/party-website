fbda5bed7b80f9794e71e1e6847edb924e2abb18
Karl <toom.karl42@gmail.com>
2023-04-30 18:23:07 +0300

    almost finished contact

--- app.js
+++ app.js
@@ -29,9 +29,18 @@ menuLink.addEventListener("click", function (event) {
   event.preventDefault();
 });
 
-const googleMaps = document.querySelector(".google__maps");
+const googleMaps = document.querySelector(".google__maps--map");
 const googleButton = document.querySelector(".contact__nav--google");
+const contactForm = document.querySelector(".contact__form--form");
+const contactButton = document.querySelector(".contact__nav--contact");
 
 googleButton.addEventListener("click", function () {
-  googleMaps.classList.toggle("active");
+  if (!googleButton.classList.contains("disabled")) {
+    googleButton.classList.add("disabled");
+    googleMaps.classList.toggle("active");
+    if (googleButton.classList.contains("active")) {
+      contactForm.classList.toggle("inactive");
+    }
+  }
 });
+
