fce119ef4503a71a5b05c900a25faaa6498f4a3b
Karl <toom.karl42@gmail.com>
2023-05-01 18:48:30 +0300

    made navlinks lightup when in section

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/* NAVIGATION BAR*/
+//STICKY NAVBAR
 const nav = document.querySelector("nav");
 const sticky = nav.offsetTop;
 
@@ -8,7 +10,7 @@ window.onscroll = function () {
     nav.classList.remove("sticky");
   }
 };
-
+//BURGER MENU
 const menuIcon = document.querySelector(".nav__link--menu");
 const navLinks = document.querySelector(".nav__links");
 
@@ -28,7 +30,29 @@ const menuLink = document.querySelector(".nav__link--menu");
 menuLink.addEventListener("click", function (event) {
   event.preventDefault();
 });
+// NAV LINK LIGHTUP
+const sections = document.querySelectorAll("section[id]");
 
+window.addEventListener("scroll", navHighlight);
+
+function navHighlight() {
+  let scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 70;
+    sectionId = current.getAttribute("id");
+    const link = document.querySelector(
+      `.nav__container a[href="#${sectionId}"]`
+    );
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
+}
+/* ABOUT BUTTONS*/
 const googleMaps = document.querySelector(".google__maps--map");
 const googleButton = document.querySelector(".contact__nav--google");
 const contactForm = document.querySelector(".contact__form--form");
